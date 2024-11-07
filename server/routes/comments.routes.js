7cb53978d4edea2a0962b82156c46199610773ec
Santiago <stdelgado2004@gmail.com>
2024-11-07 19:00:00 -0300

    fix: update function likeComment in comment like route

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createComment, deleteComment, getComment, getComments, getLikes, updateComment } from "../controllers/Comment.controller.js";
+import { createComment, deleteComment, getComment, getComments, getLikes, likeComment, updateComment } from "../controllers/Comment.controller.js";
 import { isAdmin } from '../middleware/Admin.js';
 import { authRequired } from "../middleware/validateToken.js";
 const router = Router()
@@ -12,6 +12,6 @@ router.delete('/comment/:id',authRequired,isAdmin,deleteComment)
 router.patch('/comment/:id',authRequired,updateComment)
 router.get('/comments',getComments)
 
-router.patch('/comment/:id/like', authRequired, updateComment)
+router.patch('/comment/:id/like', authRequired, likeComment)
 router.get('/comment/:id/likes', getLikes)
 export default router
